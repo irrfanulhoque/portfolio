d6212b86ba2fc5703fedaaaee7bc3b29a71b4416
irrfanulhoque <irrfanulhoque@gmail.com>
2026-01-05 21:13:29 +0600

    fix: Update Navbar name for consistency

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16">
                     <div className="flex-shrink-0">
-                        <a href="#" className="text-accent font-bold text-xl">IRFANUL HOQUE</a>
+                        <a href="#" className="text-accent font-bold text-xl">IRFANUL -----------HOQUE</a>
                     </div>
 
                     <div className="hidden md:block">
